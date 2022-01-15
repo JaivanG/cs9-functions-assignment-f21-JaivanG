72431f020bf11dbd77e85c3babca08addb136e58
Jaivan Gordon <94643233+JaivanG@users.noreply.github.com>
2022-01-15 17:16:34 +0000

    Parts 1-4

--- part3.py
+++ part3.py
@@ -19,4 +19,14 @@ def factorial(number):
   return product
 
 def combination(n, r):  # do not change this line
-  pass  # delete the word pass when you start writing your code
+  new_n=1
+  for i in range (1, n+1):
+    new_n*=1
+  new_r=1
+  for w in range (1, r+1):
+    new_r*=w
+  n_minus_r=1
+  for y in range (1, n-r+1):
+    n_minus_r*=y
+  answer=new_n/(new_r*n_minus_r)
+  return answer
